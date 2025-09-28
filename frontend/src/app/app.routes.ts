aa956bb4ca8be175b5c8cba17bb41e579b785d53
Henri Okayama <henri.okayama@gmail.com>
2025-09-28 17:30:06 -0300

    feat: pokedex page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
 import { RegisterComponent } from './pages/register/register';
 import { PokemonsComponent } from './pages/pokemons/pokemons';
+import { PokedexComponent } from './pages/pokedex/pokedex';
+import { PokemonDetailComponent } from './pages/pokemon-detail/pokemon-detail';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'pokemons', component: PokemonsComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'pokedex', component: PokedexComponent },
+  { path: 'pokemon/:id', component: PokemonDetailComponent },
+  { path: '', redirectTo: 'pokedex', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pokedex' }
 ];
